a073de51d95554e197eb71682d990d0b963750fc
rigelalgebar <rigel.algebar@gmail.com>
2020-04-01 19:46:59 -0600

    sols lab numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,93 @@
 #1. Import the NUMPY package under the name np.
 
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2,3,5))
+print(np.random.random(30).reshape(2,3,5))
+print(np.array([np.random.random() for i in range(30)]).reshape(2,3,5))
 
 
 #4. Print a.
+
+print(a)
 
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
+b = np.ones((5,2,3))
 
 
 #6. Print b.
+
+print(b)
 
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.shape == b.shape)
+# No tienen el mismo size
 
 
 #8. Are you able to add a and b? Why or why not?
+
+print(a+b)
+# No podemos sumarlos porque no tienen el mismo size
 
 
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.transpose(b,axes=[1,2,0])
+
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+
+d=a+c
 
 
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a, "\n\n\n\n",d)
+# Los vlores de "d" son igual a loas de "a" mas uno.
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e = np.multiply(a,c)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+e == a
+# Si son iguales los array ya que el resultado anterior devuelve
+# un array llenode True values
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max();
+d_min = d.min();
+d_mean = d.mean();
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty((2,3,5))
 
 
 
@@ -75,6 +101,11 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+np.place(f, (d > d_min) & (d < d_mean), 25);
+np.place(f, (d > d_mean) & (d < d_max), 75);
+np.place(f, (d == d_mean), 50);
+np.place(f, (d == d_min), 0);
+np.place(f, (d == d_max), 100);
 
 
 
@@ -99,6 +130,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+print(d, "\n\n\n\n",f)
+
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +145,15 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+g = d.copy()
+
+g[(d > d_min) & (d < d_mean)] = 1;
+g[(d > d_mean) & (d < d_max)] = 3;
+g[(d == d_mean)] = 2;
+g[(d == d_min)] = 0;
+g[(d == d_max)] = 4;
+
+dictio = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E"}
+
+h = np.vectorize(dictio.get)(g)
