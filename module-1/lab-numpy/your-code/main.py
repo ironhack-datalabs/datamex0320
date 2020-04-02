6606ccbd95298612e7db2fd0e3a22e5d3dad0898
Romero9485 <sromer9485@gmail.com>
2020-04-02 08:19:00 -0600

    agregúe lab numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,71 @@
-#1. Import the NUMPY package under the name np.
+# 1. Import the NUMPY package under the name np.
+import numpy as np
 
+# 2. Print the NUMPY version and the configuration.
+print(np.__version__)
 
-
-#2. Print the NUMPY version and the configuration.
-
-
-
-#3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
+# 3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.random((2, 3, 5))
+
+# 4. Print a.
 
 
-#4. Print a.
+print(a)
 
+# 5. Create a 5x3x2 3-dimensional array with all values equaling 1.
+# Assign the array to variable "b"
 
+b = np.full((5, 3, 2), 1)
 
-#5. Create a 5x2x3 3-dimensional array with all values equaling 1.
-#Assign the array to variable "b"
+# 6. Print b.
+print(b)
 
+# 7. Do a and b have the same size? How do you prove that in Python code?
 
+print(a == b)
 
-#6. Print b.
+# 8. Are you able to add a and b? Why or why not?
+#NO SE PUEDE POR QUE TIENEN SIZE DIFERENTES POR LO TANTO NO SE PUEDE HACER NADA CON AMBAS.
 
+# 9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = np.transpose(b)
+print(c)
 
-#7. Do a and b have the same size? How do you prove that in Python code?
+# 10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
-
-
-#8. Are you able to add a and b? Why or why not?
-
-
-
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
-
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d = c + a
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
 
+print(d)
 
+#Ambos arreglos tienen iguales dimenciones, sin embargo se difencían en  los valores que se encuentran dentro de los arreglos
 
 #12. Multiply a and c. Assign the result to e.
-
-
+e = a*b
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+#SI SON IGUALES POR QUE AMBAS TIENEN LA MISMA SIZE YA QUE SE HIZO UNA TRANSPUESTA DE A
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+dmax=d.max()
+dmin=d.min()
+dmean=d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty((2,3,5))
 
 
 """
@@ -75,7 +78,21 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for z in range(2):
+    for y in range(3):
+        for x in range(5):
+            if d[z, y, x] > d_min and d[z, y, x] < d_mean:
+                f[z, y, x] = 25
+            elif d_mean < d[z, y, x] and d[z, y, x] < d_max:
+                f[z, y, x] = 75
+            elif d[z, y, x] == d_max:
+                f[z, y, x] = 100
+            elif d[z, y, x] == d_min:
+                f[z, y, x] = 0
+            elif d[z, y, x] == d_mean:
+                f[z, y, x] = 50
 
+print(f)
 
 
 """
@@ -84,16 +101,13 @@ For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
         [1.72201435, 1.1862918 , 1.87078449, 1.7726778 , 1.88180042]],
-
        [[1.44747908, 1.31673383, 1.02000951, 1.52218947, 1.97066381],
         [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
         [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
-
 Your f should be:
 array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  25.,  25.,  25.],
         [ 75.,  25.,  75.,  75.,  75.]],
-
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
@@ -106,9 +120,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
 array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'B',  'B',  'B'],
         [ 'D',  'B',  'D',  'D',  'D']],
-
        [[ 'B',  'B',  'B',  'B',  'E'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
