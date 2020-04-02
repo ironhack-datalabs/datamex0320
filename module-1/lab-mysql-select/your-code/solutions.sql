e450a21b443f600178be511201a8ae8c1c3d6060
EsdrasGrau <esdrascampos@mac.com>
2020-04-02 00:03:57 -0600

    uploading answers

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,50 +1,63 @@
-
-# Challenge 1 - Who Have Published What At Where?
-
-select authors.au_id AS 'Author ID', authors.au_lname as 'Last Name', authors.au_fname as 'First Name', titles.title as Title,
-publishers.pub_name as Publisher 
+Esdras
+#Challenge 1 - Who Have Published What At Where?
+select 
+    authors.au_id AS 'Author_ID', 
+    authors.au_lname as 'Last_Name', 
+    authors.au_fname as 'First_Name', 
+    titles.title as Title,
+    publishers.pub_name as Publisher 
 from authors
-inner join titleauthor
-on authors.au_id = titleauthor.au_id
-inner join titles
-on titleauthor.title_id=titles.title_id
-inner join publishers
-on titles.pub_id=publishers.pub_id
+	inner join titleauthor on authors.au_id = titleauthor.au_id
+	inner join titles on titleauthor.title_id=titles.title_id
+	inner join publishers on titles.pub_id=publishers.pub_id
 order by au_lname;
 
 
-# Challenge 2 - Who Have Published How Many At Where?
-# This code doesn't work on my computer but I replicated using other student pc.
-
+#Challenge 2 - Who Have Published How Many At Where?
 CREATE TEMPORARY TABLE challenge1
-select authors.au_id, authors.au_lname, authors.au_fname, titles.title as Title,
-publishers.pub_name as Publisher 
+select 
+	authors.au_id, 
+	authors.au_lname, 
+        authors.au_fname, 
+        titles.title,
+	publishers.pub_name 
 from authors
-inner join titleauthor
-on authors.au_id = titleauthor.au_id
-inner join titles
-on titleauthor.title_id=titles.title_id
-inner join publishers
-on titles.pub_id=publishers.pub_id
+inner join titleauthor on authors.au_id = titleauthor.au_id
+inner join titles on titleauthor.title_id=titles.title_id
+inner join publishers on titles.pub_id=publishers.pub_id
 order by au_lname;
 
-select au_id, au_lname as last_name , au_fname as first_name,
-pub_name, count(pub_name) as title_count 
+
+select au_id, au_lname as Last_name , au_fname as First_name,
+pub_name as Pub_name, count(pub_name) as Title_count 
 from challenge1
 group by au_id;
 
-# Challenge 3 - Best Selling Authors
-select authors.au_id, authors.au_fname, authors.au_lname, sum(titles.price * ytd_sales) as Total
-from authors
-left join titleauthor
-on authors.au_id = titleauthor.au_id
-left join titles
-on titleauthor.title_id=titles.title_id
-group by titles.ytd_sales
-order by authors.au_id
-Limit = 
+#Challenge 3 - Best Selling Authors
+select 
+	a.au_id, 
+	au_lname, 
+        au_fname, 
+        ti.title_id, 
+        sum(qty) as totalqty 
+from authors as a
+inner join titleauthor as t on a.au_id=t.au_id
+inner join titles as ti on t.title_id=ti.title_id
+inner join sales as s on ti.title_id=s.title_id
+group by au_id
+order by totalqty desc
+limit 3;
 
 #Challenge 4 - Best Selling Authors Ranking
-.
-
-
+select 
+    a.au_id, 
+    au_lname, 
+    au_fname, 
+    ti.title_id, 
+    sum(qty) as totalqty 
+from authors as a
+left join titleauthor as t on a.au_id=t.au_id
+left join titles as ti on t.title_id=ti.title_id
+left join sales as s on ti.title_id=s.title_id
+group by au_id
+order by totalqty desc;
